dc67ce08583ea12de2f638f7738f6be60389c6cc
Kajetan <kajetan@MacBook-Pro-Kajetan.local>
2023-01-10 21:26:06 +0100

    gps try

--- main/main.ino
+++ main/main.ino
@@ -1,53 +1,70 @@
-//YWROBOT
-//Compatible with the Arduino IDE 1.0
-//Library version:1.1
-#define second_int_value 1000
-#define red_button 2
-#define blue_button 3
-#include <LiquidCrystal_I2C.h>
+// //YWROBOT
+// //Compatible with the Arduino IDE 1.0
+// //Library version:1.1
+// #define second_int_value 1000
+// #define red_button 2
+// #define blue_button 3
+// #include <LiquidCrystal_I2C.h>
+// #include <SoftwareSerial.h>
+// #include <TinyGPS++.h>
+
+// LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+// const char build_number[16]="Build nr <<<<build_id>>>>";    //build number variable for CI pipeline
+// const char build_branch[16]="<<<<build_branch>>>>"; //git branch variable for CI pipeline
+
+// long set_time_in_seconds;
+// bool end_of_a_game = false;
+// SoftwareSerial ss(4, 3);
+// TinyGPSPlus gps;
+// void start_screen(){
+//     lcd.init();                      // initialize the lcd 
+//     lcd.backlight();    // backlight on
+//     lcd.setCursor(0,0);
+//     lcd.print(build_number);
+//     lcd.setCursor(0,1);
+//     lcd.print(build_branch);
+//     delay(second_int_value);
+// }
+// void setup()
+// {
+//     ss.begin(9600);
+//     start_screen();
+// }
+
+// void loop()
+// {
+//     while (ss.available() > 0){
+//         // get the byte data from the GPS
+//         // char gpsData[16] = ss.read();
+//         lcd.clear();
+//         lcd.setCursor(0,0);
+//         gps.encode(ss.read());
+//         lcd.print(gps.location.lng(), 6);
+//         if (gps.location.isUpdated()){
+//             lcd.clear();
+//             // lcd.setCursor(0,0);
+//             // lcd.print(gps.location.lat(), 6);
+//             lcd.setCursor(0,1);
+//             lcd.print(gps.location.lng(), 6);
+//         }
+//             delay(second_int_value);
+//     }
+// }
 #include <SoftwareSerial.h>
-#include <TinyGPS++.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-
-const char build_number[16]="Build nr <<<<build_id>>>>";    //build number variable for CI pipeline
-const char build_branch[16]="<<<<build_branch>>>>"; //git branch variable for CI pipeline
-
-long set_time_in_seconds;
-bool end_of_a_game = false;
+// The serial connection to the GPS module
 SoftwareSerial ss(4, 3);
-TinyGPSPlus gps;
-void start_screen(){
-    lcd.init();                      // initialize the lcd 
-    lcd.backlight();    // backlight on
-    lcd.setCursor(0,0);
-    lcd.print(build_number);
-    lcd.setCursor(0,1);
-    lcd.print(build_branch);
-    delay(second_int_value);
-}
-void setup()
-{
-    ss.begin(9600);
-    start_screen();
+
+void setup(){
+  Serial.begin(9600);
+  ss.begin(9600);
 }
 
-void loop()
-{
-    while (ss.available() > 0){
-        // get the byte data from the GPS
-        // char gpsData[16] = ss.read();
-        lcd.clear();
-        lcd.setCursor(0,0);
-        gps.encode(ss.read());
-        lcd.print(gps.location.lng(), 6);
-        if (gps.location.isUpdated()){
-            lcd.clear();
-            // lcd.setCursor(0,0);
-            // lcd.print(gps.location.lat(), 6);
-            lcd.setCursor(0,1);
-            lcd.print(gps.location.lng(), 6);
-        }
-            delay(second_int_value);
-    }
+void loop(){
+  while (ss.available() > 0){
+    // get the byte data from the GPS
+    byte gpsData = ss.read();
+    Serial.write(gpsData);
+  }
 }
