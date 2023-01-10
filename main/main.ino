0c5a669a47892c52a20ceba18d6a09035ec66310
Kajetan <kajetan@MacBook-Pro-Kajetan.local>
2023-01-10 21:30:41 +0100

    gps try

--- main/main.ino
+++ main/main.ino
@@ -51,20 +51,37 @@
 //             delay(second_int_value);
 //     }
 // }
+/*
+ * Rui Santos 
+ * Complete Project Details https://randomnerdtutorials.com
+ */
+ 
+#include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
-// The serial connection to the GPS module
-SoftwareSerial ss(4, 3);
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
 
 void setup(){
   Serial.begin(9600);
-  ss.begin(9600);
+  ss.begin(GPSBaud);
 }
 
 void loop(){
+  // This sketch displays information every time a new sentence is correctly encoded.
   while (ss.available() > 0){
-    // get the byte data from the GPS
-    byte gpsData = ss.read();
-    Serial.write(gpsData);
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()){
+      Serial.print("Latitude= "); 
+      Serial.print(gps.location.lat(), 6);
+      Serial.print(" Longitude= "); 
+      Serial.println(gps.location.lng(), 6);
+    }
   }
 }
